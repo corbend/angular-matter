2c5d587c05ae8fe9583551820518636637cd9991
nemtarev <thinkandwin5000@gmail.com>
2015-11-27 00:59:36 +0300

    2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var mainApp = angular.module('parklon', ['ngRoute', 'ngAria', 'ngAnimate', 'ngMaterial', 'ui.grid'])
 
-mainApp.controller('mainCtrl', function($scope, $http, uiGridConstants) {
+mainApp.controller('mainCtrl', function($scope, $http, uiGridConstants, $mdDialog) {
 	$scope.test = "1";
 	$scope.gridOptions = {
 		data: [{name: 'A'}, {name: 'B'}, {name: 'C'}],
@@ -12,5 +12,29 @@ mainApp.controller('mainCtrl', function($scope, $http, uiGridConstants) {
 			{width: 120, name: 'C', field: 'name'},
 			{width: 120, name: 'D', field: 'name'}
 		]
+	}
+
+	$scope.breadcrumbs = {
+		crumbs: [
+			'Home', 'Products', 'Details'
+		]
+	}
+
+	$scope.showConfirm = function($event) {
+
+		var dialog = {
+			title: 'Удаление записи',
+			content: 'Вы уверены что хотите удалить запись?',
+			ok: 'Yes',
+			cancel: 'No'
+		}
+		$mdDialog.show(
+            $mdDialog.confirm()
+            .title(dialog.title)
+            .content(dialog.content)
+            .ok(dialog.ok)
+            .cancel(dialog.cancel)
+            .targetEvent($event)
+        );
 	}
 })
